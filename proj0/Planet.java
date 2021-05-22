e999f3e6fbd3269f308be394aa2938bf3a4d6181
Yang Ming <mingyang@Mac-2.local>
2021-05-22 17:04:11 -0400

    modify Planet.java for API check,private G

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -7,7 +7,8 @@ public double yyVel; //Its current velocity in the y direction
 public double mass; // Its mass
 public String imgFileName;
 
-public static double G=6.67e-11;
+//public static double G=6.67e-11;
+private static double G=6.67e-11;
 
 public Planet(double xP, double yP, double xV,
               double yV, double m, String img){
